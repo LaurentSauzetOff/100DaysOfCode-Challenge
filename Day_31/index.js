aba7bddadb0ba76ee992e4f97933814e1ae42499
Laurent-BC <laurent.sauzetpro@gmail.com>
2021-11-11 19:42:48 +0100

    First commit day_31

---
+++ Day_31/index.js
@@ -0,0 +1,13 @@
+let age = 32;
+let userName = "Laurent";
+let hobbies = [
+  "Cinema" /* index 0 */,
+  "Musique" /* index 1 */,
+  "Jeux vidéo" /* index 2 */,
+];
+let job = {
+  title: 'Developpeur Fullstack',
+  place: 'Lorient',
+};
+console.log(job.title);
+console.log(job.place)
