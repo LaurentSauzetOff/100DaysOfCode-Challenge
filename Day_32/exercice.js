0a47e8d34308a510f9c59e3fa750899eda7664d8
Laurent-BC <laurent.sauzetpro@gmail.com>
2021-11-21 20:23:34 +0100

    Exercise, and Day_32, complete !

--- Day_32/exercice.js
+++ Day_32/exercice.js
@@ -8,14 +8,45 @@
 //    - A variable that stores the name of an online course of your choice
 //    - A variable that stores the price of that course
 //    - A variable that stores the three main goals that you have, when taking this course
+
+/* let courseName = "Web development : the complete guide";
+let coursePrice = 39;
+let cousrGoals = ['Learn web development', 'Become a web developer', 'Have fun  !']; */
+
 // 2) Output ("alert") the three variable values
+
+/* alert(courseName)
+alert(coursePrice)
+alert(cousrGoals) */
+
 // 3) Try "grouping" the three variables together and still output their values thereafter
+
+let onlineCourse = {
+  name: "Web development : the complete guide",
+  price: 39,
+  goals: ["Learn web development", "Become a web developer", "Have fun  !"],
+};
+
+alert(onlineCourse.name);
+alert(onlineCourse.price);
+alert(onlineCourse.goals);
+
 // 4) Also output the second element in your "main goals" variable
+alert(onlineCourse.goals[1]);
 // 5) Add a custom command that does the following:
 //    - Use your "main goals" variable and access an element by its identifier
-//    - The concrete identifier value should be dynamic / flexible 
+//    - The concrete identifier value should be dynamic / flexible
 //      (i.e. the command can be executed for different identifier)
-//    - The "main goals" variable should also be dynamic: The command should work 
+//    - The "main goals" variable should also be dynamic: The command should work
 //      with ANY list of values
 //    - The custom command should provide the accessed value (i.e. the list element)
+
+function accessListItem(array, arrayIndex) {
+  let arrayElement = array[arrayIndex];
+  return arrayElement;
+}
+
 // 6) Execute your custom command from (5) and output ("alert") the result
+
+let firstGoal = accessListItem(onlineCourse.goals, 0);
+alert(firstGoal)
