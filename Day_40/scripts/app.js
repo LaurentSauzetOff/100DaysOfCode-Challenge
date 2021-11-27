041c2fef1997d280268f855a0d9e1cefdce652b7
Laurent-BC <laurent.sauzetpro@gmail.com>
2021-11-27 18:48:58 +0100

    Handling Form Submission With JavaScript

--- Day_40/scripts/app.js
+++ Day_40/scripts/app.js
@@ -1,5 +1,6 @@
 const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
+const formElement = document.querySelector('form');
 
 const editPLayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPLayer2BtnElement = document.getElementById("edit-player-2-btn");
@@ -7,5 +8,8 @@ const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
 
 editPLayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPLayer2BtnElement.addEventListener("click", openPlayerConfig);
+
 cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
+
+formElement.addEventListener('submit', savePlayerConfig)
