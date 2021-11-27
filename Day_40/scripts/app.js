57b832ed7d7e9b1319da2eb0dc84f2994d405c48
Laurent-BC <laurent.sauzetpro@gmail.com>
2021-11-27 18:19:58 +0100

    Adding JavaScript, Script Files & First Event Listeners

---
+++ Day_40/scripts/app.js
@@ -0,0 +1,6 @@
+const editPLayer1BtnElement = document.getElementById("edit-player-1-btn");
+const editPLayer2BtnElement = document.getElementById("edit-player-2-btn");
+
+
+
+editPLayer1BtnElement.addEventListener('click', openPlayerConfig);
