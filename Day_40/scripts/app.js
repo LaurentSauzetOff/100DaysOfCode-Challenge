758e8ce1aa79da85373a1411b0745666554de464
Laurent-BC <laurent.sauzetpro@gmail.com>
2021-11-27 20:16:57 +0100

    Validating User Input With JavaScript

--- Day_40/scripts/app.js
+++ Day_40/scripts/app.js
@@ -1,6 +1,7 @@
 const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
 const formElement = document.querySelector('form');
+const errorOutputElement = document.getElementById('config-error')
 
 const editPLayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPLayer2BtnElement = document.getElementById("edit-player-2-btn");
