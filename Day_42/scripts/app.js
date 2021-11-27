2d617fbcb0604c3f282e44b85b0bee837a4c0165
Laurent-BC <laurent.sauzetpro@gmail.com>
2021-11-27 20:42:35 +0100

    Storing & Managing Submitted Data

--- Day_42/scripts/app.js
+++ Day_42/scripts/app.js
@@ -1,7 +1,14 @@
+let editedPlayer = 0;
+
+const players = [
+  { name: "", symbol: "X" },
+  { name: "", symbol: "O" },
+];
+
 const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
-const formElement = document.querySelector('form');
-const errorOutputElement = document.getElementById('config-error')
+const formElement = document.querySelector("form");
+const errorOutputElement = document.getElementById("config-error");
 
 const editPLayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPLayer2BtnElement = document.getElementById("edit-player-2-btn");
@@ -13,4 +20,4 @@ editPLayer2BtnElement.addEventListener("click", openPlayerConfig);
 cancelConfigBtnElement.addEventListener("click", closePlayerConfig);
 backdropElement.addEventListener("click", closePlayerConfig);
 
-formElement.addEventListener('submit', savePlayerConfig)
+formElement.addEventListener("submit", savePlayerConfig);
