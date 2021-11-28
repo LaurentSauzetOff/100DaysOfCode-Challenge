bd8dcf59acf2e3f81f49b69a51ccc6ab3d3dbd63
Laurent-BC <laurent.sauzetpro@gmail.com>
2021-11-28 11:21:52 +0100

    Managing Game Rounds (Turns) & Field Selection

--- Day_43/scripts/app.js
+++ Day_43/scripts/app.js
@@ -1,5 +1,7 @@
 let editedPlayer = 0;
 
+let activePlayer = 0;
+
 const players = [
   { name: "", symbol: "X" },
   { name: "", symbol: "O" },
@@ -9,12 +11,14 @@ const playerConfigOverlayElement = document.getElementById("config-overlay");
 const backdropElement = document.getElementById("backdrop");
 const formElement = document.querySelector("form");
 const errorOutputElement = document.getElementById("config-error");
-const gameAreaElement = document.getElementById('active-game')
+const gameAreaElement = document.getElementById("active-game");
+const activePlayerNameElement = document.getElementById("active-player-name");
 
 const editPLayer1BtnElement = document.getElementById("edit-player-1-btn");
 const editPLayer2BtnElement = document.getElementById("edit-player-2-btn");
 const cancelConfigBtnElement = document.getElementById("cancel-config-btn");
 const startNewGameBtnElement = document.getElementById("start-game-btn");
+const gameFieldElements = document.querySelectorAll("#game-board li");
 
 editPLayer1BtnElement.addEventListener("click", openPlayerConfig);
 editPLayer2BtnElement.addEventListener("click", openPlayerConfig);
@@ -25,3 +29,7 @@ backdropElement.addEventListener("click", closePlayerConfig);
 formElement.addEventListener("submit", savePlayerConfig);
 
 startNewGameBtnElement.addEventListener("click", startNewGame);
+
+for (const gameFieldElement of gameFieldElements) {
+  gameFieldElement.addEventListener("click", selectGameField);
+}
