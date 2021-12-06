c509ed8b2bc6a315181c6c53ace7e4218dbafd24
Laurent-BC <laurent.sauzetpro@gmail.com>
2021-12-06 16:49:54 +0100

    Mon premier server node !!!

--- Day_47/app.js
+++ Day_47/app.js
@@ -1,2 +1,18 @@
-const userName = "Laurent";
-console.log(userName);
+const http = require("http");
+
+function handleRequest(request, response) {
+  if (request.url === "/currenttime") {
+    response.statusCode = 200;
+    response.end("<h1>" + new Date().toISOString() + "</h1>");
+  } else if (request.url === "/") {
+    response.statusCode = 200;
+    response.end("<h1>Coucou le monde !!!!!!!!!!</h1>");
+  }
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(443);
+
+// amazon.com => envoie d'une requete au serveur amazon
+// amazon.com:80
