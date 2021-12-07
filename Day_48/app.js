86ea060e4ad6bec302b7752f5492ebdf84af4845
Laurent-BC <laurent.sauzetpro@gmail.com>
2021-12-07 19:00:02 +0100

    Storing data in server-side files

--- Day_48/app.js
+++ Day_48/app.js
@@ -1,18 +1,29 @@
-const http = require("http");
+const fs = require('fs');
 
-function handleRequest(request, response) {
-  if (request.url === "/currenttime") {
-    response.statusCode = 200;
-    response.end("<h1>" + new Date().toISOString() + "</h1>");
-  } else if (request.url === "/") {
-    response.statusCode = 200;
-    response.end("<h1>Coucou le monde !!!!!!!!!!</h1>");
-  }
-}
+const path = require('path');
 
-const server = http.createServer(handleRequest);
+const express = require("express");
 
-server.listen(443);
+const app = express();
 
-// amazon.com => envoie d'une requete au serveur amazon
-// amazon.com:80
+app.use(express.urlencoded({extended: false}));
+
+app.get("/currenttime", function (request, response) {
+  response.send("<h1>" + new Date().toISOString() + "</h1>");
+}); // localhost:443/currenttime
+
+app.get("/", function (request, response) {
+  response.send('<form action="/store-user" method="POST"><label></label>Your name<input name="username" type="text"><button>Submit</button></form>');
+}); // localhost:443/
+
+app.post('/store-user', function(req, res){
+ const userName = req.body.username;
+ const filePath = path.join(__dirname, 'data', 'users.json');
+ const fileData = fs.readFileSync(filePath);
+ const existingUsers = JSON.parse(fileData);
+ existingUsers.push(userName);
+ fs.writeFileSync(filePath, JSON.stringify(existingUsers));
+ res.send('<h1>Username stored !</h1>')
+})
+
+app.listen(443);
