e3f26bdfd2fd0497927867ac191d2a1a65eca756
Laurent-BC <laurent.sauzetpro@gmail.com>
2021-12-09 19:53:51 +0100

    Parsing form data and redirecting requests

--- Day_50/app.js
+++ Day_50/app.js
@@ -1,11 +1,13 @@
+const fs = require("fs");
+
 const path = require("path");
 
 const express = require("express");
 
 const app = express();
 
-app.use(express.static('public'));
-
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
   const htmlFilePath = path.join(__dirname, "views", "index.html");
@@ -20,6 +22,20 @@ app.get("/restaurants", function (req, res) {
 app.get("/recommend", function (req, res) {
   const htmlFilePath = path.join(__dirname, "views", "recommend.html");
   res.sendFile(htmlFilePath);
+});
+
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  const filePath = path.join(__dirname, "data", "restaurant.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  storedRestaurants.push(restaurant);
+
+  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+  res.redirect("/confirm");
 });
 
 app.get("/confirm", function (req, res) {
