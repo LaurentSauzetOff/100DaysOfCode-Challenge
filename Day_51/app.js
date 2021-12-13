616350e8b00b108f4cfac945d390e87fe2844a14
Laurent-BC <laurent.sauzetpro@gmail.com>
2021-12-13 20:45:45 +0100

    More 404 Page Usage (Non-Existent Routes)

--- Day_51/app.js
+++ Day_51/app.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
+const uuid = require("uuid");
 
 const app = express();
 
@@ -27,12 +28,27 @@ app.get("/restaurants", function (req, res) {
   });
 });
 
+app.get("/restaurants/:id", function (req, res) {
+  const restaurantId = req.params.id;
+  const filePath = path.join(__dirname, "data", "restaurant.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  for (const restaurant of storedRestaurants) {
+    if (restaurant.id === restaurantId) {
+      return res.render("restaurant_detail", { restaurant: restaurant });
+    }
+  }
+});
+
 app.get("/recommend", function (req, res) {
   res.render("recommend");
 });
 
 app.post("/recommend", function (req, res) {
   const restaurant = req.body;
+  restaurant.id = uuid.v4();
   const filePath = path.join(__dirname, "data", "restaurant.json");
 
   const fileData = fs.readFileSync(filePath);
