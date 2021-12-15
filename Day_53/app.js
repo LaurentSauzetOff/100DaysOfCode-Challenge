d4114a5b9339b8ef8838c52a3279fe1d0f47faf6
Laurent-BC <laurent.sauzetpro@gmail.com>
2021-12-15 11:22:56 +0100

    First commit Day_53

---
+++ Day_53/app.js
@@ -0,0 +1,79 @@
+const fs = require("fs");
+
+const path = require("path");
+
+const express = require("express");
+const uuid = require("uuid");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.get("/restaurants", function (req, res) {
+  const filePath = path.join(__dirname, "data", "restaurant.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+  res.render("restaurants", {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants,
+  });
+});
+
+app.get("/restaurants/:id", function (req, res) {
+  const restaurantId = req.params.id;
+  const filePath = path.join(__dirname, "data", "restaurant.json");
+
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  for (const restaurant of storedRestaurants) {
+    if (restaurant.id === restaurantId) {
+      return res.render("restaurant_detail", { restaurant: restaurant });
+    }
+  }
+
+  res.status(404).render("404");
+});
+
+app.get("/recommend", function (req, res) {
+  res.render("recommend");
+});
+
+app.post("/recommend", function (req, res) {
+  const restaurant = req.body;
+  restaurant.id = uuid.v4();
+  const restaurants = getStoredRestaurants();
+
+  restaurants.push(restaurant);
+
+  storeRestaurants(restaurants);
+
+  res.redirect("/confirm");
+});
+
+app.get("/confirm", function (req, res) {
+  res.render("confirm");
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.use(function (req, res) {
+  res.status(404).render("404");
+});
+
+app.use(function (error, req, res, next) {
+  res.status(500).render("500");
+});
+
+app.listen(443);
